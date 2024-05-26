48fac740ae126f31147ab83d3230581dee690f6b
chansik0504 <chansik0504@gmail.com>
2024-05-26 21:12:50 +0900

    '24/05/20'

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,10 @@ import ListsContainer from './components/ListsContainer/ListsContainer';
 import { useTypedDispatch, useTypedSelector } from './hooks/redux';
 import EditModal from './components/ModalEdit/EditModal';
 import LoggerModal from './components/LoggerModal/LoggerModal';
-import { deleteBoard } from './store/slices/boardsSlice';
+import { deleteBoard, sort } from './store/slices/boardsSlice';
 import { v4 } from 'uuid';
 import { addLog } from './store/slices/loggerSlice';
+import { DragDropContext } from '@hello-pangea/dnd';
 
 function App() {
   const dispatch = useTypedDispatch();
@@ -49,13 +50,51 @@ function App() {
     }
   }
 
+  const handleDragEnd = (result: any) => {
+    console.log(result);
+    const { destination, source, draggableId } = result;
+    console.log('list', lists);
+    const sourceList = lists.filter(
+      list => list.listId === source.droppableId
+    )[0];
+
+    console.log('source list', sourceList);
+
+    dispatch(
+      sort({
+        boardIndex: boards.findIndex(board => board.boardId === activeBoardId),
+        droppableIdStart: source.droppableId,
+        droppableIdEnd: destination.droppableId,
+        droppableIndexStart: source.index,
+        droppableIndexEnd: destination.index,
+        draggableId
+      })
+    )
+
+    dispatch(
+      addLog({
+        logId: v4(),
+        logMessage: `
+        리스트 "${sourceList.listName}" 에서
+        리스트 "${lists.filter(list => list.listId === destination.droppableId)[0].listName}으로
+        ${sourceList.tasks.filter(task => task.taskId === draggableId)[0].taskName}을 옮김.
+        `,
+        logAuthor: "User",
+        logTimestamp: String(Date.now()),
+      })
+    )
+    
+  }
+
   return (
     <div className={appContainer}>
       {isLoggerOpen ? <LoggerModal setIsLoggerOpen={setIsLoggerOpen}/> : null}
       {modalActive ? <EditModal /> : null}
       <BoardList activeBoardId={activeBoardId} setActiveBoardId={setActiveBoardId}/>
       <div className={board}>
-        <ListsContainer lists={lists} boardId={getActiveBoard.boardId} />
+        <DragDropContext onDragEnd={handleDragEnd}>
+          <ListsContainer lists={lists} boardId={getActiveBoard.boardId} />
+        </DragDropContext>
       </div>
       <div className={buttons}>
         <button className={deleteBoardButton} onClick={handleDeleteBoard}>
---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCo6kn0XO2PfWJvY3y145GjpS-UubUSDUo",
+    authDomain: "react-test-app-2-13a02.firebaseapp.com",
+    projectId: "react-test-app-2-13a02",
+    storageBucket: "react-test-app-2-13a02.appspot.com",
+    messagingSenderId: "805364076263",
+    appId: "1:805364076263:web:db5b5109edfb6956bf1c42"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
